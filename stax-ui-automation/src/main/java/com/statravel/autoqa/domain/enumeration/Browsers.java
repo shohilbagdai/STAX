ad7412050485f27ee4b2c866590fe4348a87df4e
shohilbagdai <shohil.bagdai@statravel.com>
2018-04-13 15:53:30 +0100

    First commint

--- stax-ui-automation/src/main/java/com/statravel/autoqa/domain/enumeration/Browsers.java
+++
@@ -1,5 +0,0 @@
-package com.statravel.autoqa.domain.enumeration;
-
-public enum Browsers {
-	CHROME, FIREFOX, IE, SAFARI
-}
--- stax-ui-automation/src/main/java/com/statravel/autoqa/domain/enumeration/POS.java
+++
@@ -1,9 +0,0 @@
-package com.statravel.autoqa.domain.enumeration;
-
-/**
- * @author STA Development Team
- *
- */
-public enum POS {
-    AU, NZ, UK, USA
-}
